22bbe4958637b97693530c20e97a0ced19d7f218
e.debie <evadebie@oulook.com>
2025-06-12 01:42:59 +0200

    Added saving mechanisms which should work (and do work for a part of the measurements). Leaving overnight to record all of them.

--- Project_IMAU/measurement_separator.py
+++ Project_IMAU/measurement_separator.py
@@ -24,32 +24,86 @@ def derivative(arr, times):
     return out
 
 
-if __name__=='__main__':
+def main(mz_index, plot=False):
     files = rfr.file_list()
     tot_time = []
     tot_data = []
-    for fname in files[:10]:
+    for fname in files:
         r_data = rfr.read_file(fname)
         times = rfr.get_times(r_data)
-        col = r_data.iloc[:,169]
+        col = r_data.iloc[:,mz_index]
         for i,j in zip(col,times):
             tot_time.append(j)
             tot_data.append(i)
 
+    tot_mz = rfr.get_mzs(r_data)
     tot_data = np.asarray(tot_data)
     tot_time = np.asarray(tot_time)
-    print(f'm/z = {rfr.get_mzs(r_data)[169]}')
+    print(f'm/z = {tot_mz[mz_index]}')
 
     peaks, peak_details = sp.signal.find_peaks(tot_data, prominence=.5, height=.2, width=2, rel_height=.8)
-    print(peak_details)
+
+    if plot:
+        fig, ax = plt.subplots()
+        ax.plot(tot_time, tot_data, '-')
+        for i,peak in enumerate(peaks):
+            ax.vlines(tot_time[peak], ymin=0, ymax=tot_data[peak], colors='#B00B69')
+            ax.fill_betweenx(y=np.linspace(0, tot_data[peak], 1000),
+                             x1=tot_time[round(peak_details['left_ips'][i])],
+                             x2=tot_time[round(peak_details['right_ips'][i])],
+                             alpha=0.5)
+
+    output_time = []
+    output_data = []
+    for i in range(len(peaks)):
+        output_time.append(tot_time[round(peak_details['left_ips'][i]):round(peak_details['right_ips'][i])])
+        output_data.append(tot_data[round(peak_details['left_ips'][i]):round(peak_details['right_ips'][i])])
+
+    return output_time, output_data
 
 
-    fig, ax = plt.subplots()
-    ax.plot(tot_time, tot_data, '-')
-    for i,peak in enumerate(peaks):
-        ax.vlines(tot_time[peak], ymin=0, ymax=tot_data[peak], colors='#B00B69')
-        ax.fill_betweenx(y=np.linspace(0, tot_data[peak], 1000),
-                         x1=tot_time[round(peak_details['left_ips'][i])],
-                         x2=tot_time[round(peak_details['right_ips'][i])],
-                         alpha=0.5)
+
+if __name__=='__main__':
+    all_files = rfr.file_list()
+    first_file = rfr.read_file(all_files[0])
+    all_mzs = rfr.get_mzs(first_file)
+    all_times = []
+    all_data = []
+    for i in range(len(all_mzs)):
+        current_times, current_data = main(i)
+        all_times.append(current_times)
+        all_data.append(current_data)
+
+
+    final_times = []
+    final_data = []
+    for i,(j,k) in enumerate(zip(all_times,all_data)):
+        current_final_times = []
+        for jj in j:
+            current_final_times.append(np.mean(jj))
+        current_final_data = []
+        for kk in k:
+            current_final_data.append(np.mean(kk))
+
+        final_times.append(current_final_times)
+        final_data.append(current_final_data)
+
+    file_data = open('extracted_data.csv', 'w')
+    file_data.write('')
+    file_data.close()
+
+    file_time = open('extracted_time.csv', 'w')
+    file_time.write('')
+    file_time.close()
+
+    for i,j,k in zip(all_mzs, final_data, final_times):
+        print(i,j,k)
+        file_data = open('extracted_data.csv', 'a')
+        file_data.writelines(f'{i};{j}\n')
+        file_data.close()
+
+        file_time = open('extracted_time.csv', 'a')
+        file_time.writelines(f'{i};{k}\n')
+        file_time.close()
+
     plt.show()
