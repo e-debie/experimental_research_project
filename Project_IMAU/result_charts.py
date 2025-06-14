b4e2aef8cda3b0db5172b1d75b7fe41f288fdb85
e.debie <evadebie@oulook.com>
2025-06-15 01:24:57 +0200

    Poster v1.5, added ocean plastic concentrations

--- Project_IMAU/result_charts.py
+++ Project_IMAU/result_charts.py
@@ -14,16 +14,17 @@ A = data['A']
 B = data['B']
 C = data['C']
 D = data['D']
+OCEAN = np.asarray(data['Ocean'])
 own_test = data['OT']
 
 production_data = data['WorldProduction']
 
-rows = [' ', '  ', '   ', '    ', '     ']
-masses = np.asarray([14.0632, 16.5189, 14.2263, 22.3569, 14.2263])
+rows = ['  ', '   ', '    ', '     ']
+masses = np.asarray([14.0632, 16.5189, 14.2263, 22.3569])
 volumes = [50 for i in masses]
 
-values = np.zeros((5,len(plastics_order)))
-for i,(j,k,l) in enumerate(zip(np.vstack((A,C,D,B,own_test)), masses, volumes)):
+values = np.zeros((4,len(plastics_order)))
+for i,(j,k,l) in enumerate(zip(np.vstack((A,C,D,B)), masses, volumes)):
     values[i] = j*l/k
 values_cum = values.cumsum(axis=1)
 
@@ -33,6 +34,14 @@ fig, ax = plt.subplots()
 ax.invert_yaxis()
 ax.xaxis.set_visible(False)
 ax.set_xlim(0, np.sum(values, axis=1).max())
+
+OCEAN_cum = OCEAN.cumsum()
+for i,(color,colname) in enumerate(zip(colours, plastics_order)):
+    widths = OCEAN[i]
+    starts = OCEAN_cum[i] - widths
+    rects = ax.barh(' ', widths, left=starts, height=0.8, color=color)
+    plth.start(20)
+plth.start(24)
 
 for i,(color,colname) in enumerate(zip(colours, plastics_order)):
     widths = values[:,i]
@@ -46,22 +55,30 @@ plth.start(24)
 plt.legend()
 plt.title('Plastic distribution across depth (ng/g)')
 
-figpies, axpies = plt.subplots(3,2)
 
-axpies[0,0].pie(A, labels=plastics_order, colors=colours, autopct='%1.1f%%')
-plt.suptitle('Plastic fractionation in surface')
+plth.start(20)
 
-axpies[0,1].pie(B, labels=plastics_order, colors=colours, autopct='%1.1f%%')
+figpies, axpies = plt.subplots(2,2)
 
-axpies[1,0].pie(C, labels=plastics_order, colors=colours, autopct='%1.1f%%')
+plt.suptitle('Plastic Fractionation')
 
-axpies[2,1].pie(D, labels=plastics_order, colors=colours, autopct='%1.1f%%')
+axpies[1,0].pie(A, labels=plastics_order, colors=colours, autopct='%1.1f%%')
+axpies[1,0].set_title('Surface layer')
 
-axpies[1,1].pie(own_test, labels=plastics_order, colors=colours, autopct='%1.1f%%')
-axpies[1,1].set_title('Own calculation')
+# axpies[0,1].pie(B, labels=plastics_order, colors=colours, autopct='%1.1f%%')
+#
+# axpies[1,0].pie(C, labels=plastics_order, colors=colours, autopct='%1.1f%%')
+#
+axpies[1,1].pie(D, labels=plastics_order, colors=colours, autopct='%1.1f%%')
+axpies[1,1].set_title('Deepest tested layer')
+#
+# axpies[1,1].pie(own_test, labels=plastics_order, colors=colours, autopct='%1.1f%%')
+# axpies[1,1].set_title('Own calculation')
 
-fig_prod, ax_prod = plt.subplots()
-ax_prod.pie(production_data, labels=plastics_order, colors=colours)
-plt.suptitle('Relevant plastic fractionation in production [4]')
+axpies[0,1].pie(production_data, labels=plastics_order, colors=colours, autopct='%1.1f%%')
+axpies[0,1].set_title('Production [4]')
+
+axpies[0,0].pie(OCEAN, labels=plastics_order, colors=colours, autopct='%1.1f%%')
+axpies[0,0].set_title('Deep ocean')
 
 plt.show()
